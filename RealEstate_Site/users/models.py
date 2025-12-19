0cbe97b2274dcb5d75665fe8f86e5183d042c014
mumer2650 <2024cs191@student.uet.edu.pk>
2025-12-19 18:13:08 +0500

    User role added

--- RealEstate_Site/users/models.py
+++ RealEstate_Site/users/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('customer', 'Customer'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='buyer')
     is_agent = models.BooleanField(default=False) 
     
     def __str__(self):
--- RealEstate_Site/users/views.py
+++ RealEstate_Site/users/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer
+from .permissions import IsAdminRole
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
