16b1d9d780b4a80d33caffa5349a8e21bca8dd59
M-HassanRaza724 <2024cs213@student.uet.edu.pk>
2026-01-04 01:08:41 +0500

    conflicts resolving

--- RealEstate_Site/api/urls.py
+++ RealEstate_Site/api/urls.py
@@ -5,6 +5,6 @@ from django.urls import path , include
 urlpatterns = [
     path('', include('users.urls')),
     path('properties/', include('listing.urls')),
-    path('location/',include('locations.urls')),
+    path('locations/',include('locations.urls')),
     path('homebuilder/',include('homebuilder.urls')),
 ]
